20615b8e6a777976dff0b9605805d95b2be2fc55
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-27 23:33:31 +0100

    Toast

--- app/src/main/java/sb/illbethere/MainActivity.java
+++ app/src/main/java/sb/illbethere/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,4 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         myDatabaseHelper = new DatabaseHelper(this);
     }
+
+    public void AddData(String newEntry){
+        boolean insertData = myDatabaseHelper.addData(newEntry);
+
+        if (insertData){
+            toastMessage("Data inserted");
+        }
+        else{
+            toastMessage("No");
+        }
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
