edd4f8b35f77b26b977a19bd9f21ecc11672667c
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-28 00:44:26 +0100

    On click listeners added

--- app/src/main/java/sb/illbethere/MainActivity.java
+++ app/src/main/java/sb/illbethere/MainActivity.java
@@ -1,7 +1,9 @@
 package sb.illbethere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,6 +24,28 @@ public class MainActivity extends AppCompatActivity {
         btnView = (Button) findViewById(R.id.btnView);
 
         myDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() !=0){
+                    AddData(newEntry);
+                    editText.setText("");
+                }
+                else {
+                    toastMessage("Insert Something");
+                }
+            }
+        });
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void AddData(String newEntry){
