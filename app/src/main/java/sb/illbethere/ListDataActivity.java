ceb8eafa17ac103cffb716b5100f054937747d96
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-29 00:50:00 +0100

    List View now implemented. ArrayList looping

--- app/src/main/java/sb/illbethere/ListDataActivity.java
+++ app/src/main/java/sb/illbethere/ListDataActivity.java
@@ -1,8 +1,14 @@
 package sb.illbethere;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sean on 28/06/2017.
@@ -25,6 +31,17 @@ public class ListDataActivity extends AppCompatActivity {
     }
 
     private void populateListView(){
-
+        Cursor data = myDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+        while (data.moveToNext()){
+            listData.add(data.getString(1));
+        }
+        ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        myListView.setAdapter(listAdapter);
     }
+
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
 }
