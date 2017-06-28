2c878fa84b02cbdccc8724c6d0203dc72134c51e
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-28 18:46:57 +0100

    ListView and new list class

--- app/src/main/java/sb/illbethere/MainActivity.java
+++ app/src/main/java/sb/illbethere/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         btnView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ListDataActivity class);
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
                 startActivity(intent);
             }
         });
