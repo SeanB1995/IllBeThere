f722750140d9aeb325c665a0d7a10dcd27604d84
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-29 00:03:36 +0100

    More methods

--- app/src/main/java/sb/illbethere/ListDataActivity.java
+++ app/src/main/java/sb/illbethere/ListDataActivity.java
@@ -2,15 +2,29 @@ package sb.illbethere;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 /**
  * Created by Sean on 28/06/2017.
  */
 
 public class ListDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "ListDataActivity";
+    DatabaseHelper myDatabaseHelper;
+    private ListView myListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+        myListView = (ListView) findViewById(R.id.listview);
+        myDatabaseHelper = new DatabaseHelper(this);
+
+        populateListView();
+    }
+
+    private void populateListView(){
+
     }
 }
