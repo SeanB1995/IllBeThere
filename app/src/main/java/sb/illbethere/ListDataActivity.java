e1da43106800fd1a22c68bf0d368bf30d9a35e49
SeanB1995 <x15561177@student.ncirl.ie>
2017-06-30 00:34:34 +0100

    Minor on click added

--- app/src/main/java/sb/illbethere/ListDataActivity.java
+++ app/src/main/java/sb/illbethere/ListDataActivity.java
@@ -3,6 +3,8 @@ package sb.illbethere;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -38,6 +40,13 @@ public class ListDataActivity extends AppCompatActivity {
         }
         ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         myListView.setAdapter(listAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name;
+            }
+        });
     }
 
     private void toastMessage(String message){
