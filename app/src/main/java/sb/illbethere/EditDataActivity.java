72825cd3d4cf1e0f57aee7a9fbfdf9273d278719
SeanB1995 <x15561177@student.ncirl.ie>
2017-07-02 00:17:53 +0100

    EditDataActivity created

---
+++ app/src/main/java/sb/illbethere/EditDataActivity.java
@@ -0,0 +1,17 @@
+package sb.illbethere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Sean on 02/07/2017.
+ */
+
+public class EditDataActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
