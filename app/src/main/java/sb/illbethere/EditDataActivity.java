6b9e7726eb95f448455c2c39ee00f0bf4ca2e792
SeanB1995 <x15561177@student.ncirl.ie>
2017-07-09 00:16:18 +0100

    Edit Data variables declared

--- app/src/main/java/sb/illbethere/EditDataActivity.java
+++ app/src/main/java/sb/illbethere/EditDataActivity.java
@@ -3,12 +3,26 @@ package sb.illbethere;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Sean on 02/07/2017.
  */
 
 public class EditDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "EditDataActivity";
+
+    private Button btnSave, btnDelete;
+    private EditText editable_Item;
+
+    DatabaseHelper myDatabaseHelper;
+
+    private String selectedName;
+    private int selectedID;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
